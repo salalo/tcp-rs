0888b0adde380485569d6fcfc7a3c049d0bc4a77
salalo <wojteksala190@gmail.com>
2022-12-12 01:53:12 +0100

    todo

--- bin/tcp-rs/src/main.rs
+++ bin/tcp-rs/src/main.rs
@@ -3,8 +3,10 @@ use std::io;
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1500];
-    let n = nic.recv(&mut buf[..])?;
-    println!("Received {:?} bytes: {:?}", n, &buf[..n]);
-
-    Ok(())
+    loop {
+        let n = nic.recv(&mut buf)?;
+        // filter out non ipv4 packets
+        // use etherpars to parse the ethernet frame
+        println!("Received {:?} bytes: {:?}", n, &buf[..n]);
+    }
 }
